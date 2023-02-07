d55802da09b2285b94d7c8bcfc683569289f7978
daemon <0xDAEF0F@proton.me>
2023-02-07 12:52:07 -0600

    forge install: solmate

--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -3,6 +3,7 @@ pragma solidity =0.8.18;
 
 import "forge-std/Test.sol";
 import "../src/Counter.sol";
+import {ERC721} from "@solmate/tokens/ERC721.sol";
 
 contract CounterTest is Test {
     Counter public counter;
